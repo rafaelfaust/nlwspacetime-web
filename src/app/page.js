dce49b349a10e9d954c2d07ae6e988fb427c9f1d
Rafael Faust Rodrigues <106990982+rafaelfaust@users.noreply.github.com>
2023-05-17 07:25:50 -0300

    aula 1

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between px-28 py-16"></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <p className="text-4xl font-bold">Sua cápsula do tempo</p>
-    </div>
-  )
-}
